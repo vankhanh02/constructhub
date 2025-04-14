0063c39d6ec6e30a5cfe37fb0bf3bd2e971ced35
vankhanh02 <khanhvvan202@gmail.com>
2025-04-14 18:52:28 +0700

    update little things

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/AppSidebar";
 import { SiteHeader } from "./components/SideHeader";
 import { Navbar } from "./components/Navbar";
+import { Dashboard } from "./pages/Dashboard";
 function App() {
   return (
     <SidebarProvider>
@@ -9,7 +10,9 @@ function App() {
       <SidebarInset>
         <SiteHeader />
         <Navbar />
-        <main></main>
+        <main>
+          <Dashboard />
+        </main>
       </SidebarInset>
     </SidebarProvider>
   );
