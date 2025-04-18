900bf6f8a059300de4bfffa347ce75d914fc432b
vankhanh02 <khanhvvan202@gmail.com>
2025-04-18 22:23:39 +0700

    add user profile

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 export const Navbar = () => {
   return (
     <nav className="w-full h-12 flex items-center px-4 border-b bg-white shadow-sm">
-      <h1 className="text-base font-medium">Công trình</h1>
+      <h1 className="text-base font-medium">Dashboard</h1>
     </nav>
   );
 };
--- frontend/src/components/SideHeader.tsx
+++ frontend/src/components/SideHeader.tsx
@@ -1,6 +1,7 @@
 import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
-
+import { Bell } from "lucide-react";
+import profileImage from "@/assets/images/profile.png";
 export function SiteHeader() {
   return (
     <header className="w-full flex h-12 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear">
@@ -12,8 +13,23 @@ export function SiteHeader() {
             className="mx-2 data-[orientation=vertical]:h-4"
           />
         </div>
-        <div>
-          <h1 className="text-base font-medium">Documents</h1>
+        <div className="flex items-center gap-3 pr-4 lg:gap-4 lg:pr-6 mr-4">
+          <Bell className="h-5 w-5 text-muted-foreground mr-1" />
+          <div className="flex items-center gap-3">
+            <div className="flex flex-col w-auto">
+              <p className="text-sm font-medium whitespace-nowrap">
+                Anima Agrawal
+              </p>
+              <p className="text-xs text-muted-foreground whitespace-nowrap">
+                U.P, India
+              </p>
+            </div>
+            <img
+              src={profileImage}
+              className="h-9 w-9 rounded-full"
+              alt="Profile"
+            />
+          </div>
         </div>
       </div>
     </header>
