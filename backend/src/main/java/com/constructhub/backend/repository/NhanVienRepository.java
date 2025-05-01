628e585ae9587d70317628c01b8d71bc09fd1bea
vankhanh02 <khanhvvan202@gmail.com>
2025-05-01 18:48:55 +0700

    add backend service

--- backend/src/main/java/com/constructhub/backend/repository/NhanVienRepository.java
+++
@@ -1,9 +0,0 @@
-package com.constructhub.backend.repository;
-
-import com.constructhub.backend.entity.NhanVien;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NhanVienRepository extends JpaRepository<NhanVien, Long> {
-}
